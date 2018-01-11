a0ceb8ef581ab86d7a299a580053b0ea14f2c061
Alexander Håkansson <hsson@brkt.com>
2018-01-11 14:49:32 -0800

    Implements simpler mailer
    Implements a mailer that can be used for sending mail, such as
    reports, to any brkt employee.
    
    Jira: None

---
+++ mailer/mailer.go
@@ -0,0 +1,80 @@
+package mailer
+
+import (
+	"bytes"
+	"fmt"
+	"net/smtp"
+	"text/template"
+)
+
+const (
+	smtpServer    = "smtp.gmail.com"
+	smtpPort      = 587
+	emailTemplate = `From: {{ .DisplayName }} <{{- .From -}}>
+To: {{ .To }}
+Subject: {{ .Subject }}
+MIME-version: 1.0;
+Content-Type: text/html; charset="UTF-8";
+
+{{ .Body }}`
+)
+
+// Client is used to send emails using standard settings
+type Client interface {
+	// SendEmail will send a mail to the specified email address
+	SendEmail(email, subject, content string) error
+}
+
+type mailer struct {
+	user        string
+	auth        smtp.Auth
+	displayName string
+}
+
+// NewClient will create a new email client for sending mails
+func NewClient(username, password, displayName string) Client {
+	auth := smtp.PlainAuth("", username, password, smtpServer)
+	m := new(mailer)
+	m.auth = auth
+	m.user = username
+	m.displayName = displayName
+
+	return m
+}
+
+// SendEmail will send a mail to the specified address. Please note that
+// the content is not HTML escaped. That would be up to whoever uses the method
+func (m *mailer) SendEmail(address, subject, content string) error {
+	server := fmt.Sprintf("%s:%d", smtpServer, smtpPort)
+	var msg bytes.Buffer
+
+	context := &mailContext{
+		From:        m.user,
+		To:          address,
+		Subject:     subject,
+		Body:        content,
+		DisplayName: m.displayName,
+	}
+
+	t := template.New("mailTemplate")
+	t, err := t.Parse(emailTemplate)
+	if err != nil {
+		return err
+	}
+
+	err = t.Execute(&msg, context)
+	if err != nil {
+		return err
+	}
+
+	err = smtp.SendMail(server, m.auth, m.user, []string{address}, msg.Bytes())
+	return err
+}
+
+type mailContext struct {
+	From        string
+	To          string
+	Subject     string
+	Body        string
+	DisplayName string
+}
